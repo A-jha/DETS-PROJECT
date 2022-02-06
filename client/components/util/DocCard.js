730d454af281d5193bee300ad306fd116d093f6a
A-Jha <avvinashjha@gmial.com>
2022-02-06 19:29:23 +0530

    re started

---
+++ client/components/util/DocCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography, Card } from "@mui/material";
+const DocCard = () => {
+  return (
+    <Card sx={{ padding: "30px" }}>
+      <Typography variant="h3">Documents-1</Typography>
+    </Card>
+  );
+};
+
+export default DocCard;
--- client/pages/docs.js
+++ client/pages/docs.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import { Typography, Grid } from "@mui/material";
+import DocCard from "../components/util/DocCard";
 const Docs = () => {
   return (
     <Layout>
-      <h1>Documents</h1>
+      <Grid container>
+        <Grid item p={5} xs={12} md={6}>
+          <DocCard />
+        </Grid>
+        <Grid item p={5} xs={12} md={6}>
+          <DocCard />
+        </Grid>
+        <Grid item p={5} xs={12} md={6}>
+          <DocCard />
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
